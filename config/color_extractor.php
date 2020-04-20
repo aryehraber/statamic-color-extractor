551c52c6190ee4e984ecdaf3b69189322890d2d9
Aryeh Raber <aryeh.raber@gmail.com>
2020-04-20 11:20:44 +0200

    Add comments to config

--- config/color_extractor.php
+++ config/color_extractor.php
@@ -1,8 +1,53 @@
 <?php
 
 return [
-    'accuracy' => 500, // in pixels; higher values will result in more accurate color extraction but will take longer to process
-    'fallback' => '#000000', // used as a fallback for transparent images
-    'temp_dir' => storage_path('color_extractor'), // images are temporarily stored here during processing
+
+    /*
+    |--------------------------------------------------------------------------
+    | Color Extraction Accuracy
+    |--------------------------------------------------------------------------
+    |
+    | This value (in pixels) determines the width/height at which the image
+    | is resized before processing. Higher values will result in more
+    | accurate color extraction but will take longer to process.
+    |
+    */
+    'accuracy' => 500,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Fallback Color
+    |--------------------------------------------------------------------------
+    |
+    | Transparent images require a fallback color since it isn't possible
+    | to extract from a color that has an alpha value.
+    |
+    */
+    'fallback' => '#000000',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Color Extraction Strategy
+    |--------------------------------------------------------------------------
+    |
+    | The are two color extraction strategies; "dominant" analyses all pixels
+    | in the image and calculates the most dominant color, while "average"
+    | reduces the image down to 1 pixel and extracts its color.
+    |
+    | Supported: "dominant", "average"
+    |
+    */
     'default_type' => 'dominant',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Temp. Storage Directory
+    |--------------------------------------------------------------------------
+    |
+    | During image processing, the image needs to temporarily be stored on
+    | the filesystem, once done it will automatically be removed.
+    |
+    */
+    'temp_dir' => storage_path('color_extractor'),
+
 ];
