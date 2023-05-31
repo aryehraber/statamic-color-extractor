d46cd8afdc8bd3bc8946fa86cec9a666838c09ae
Aryeh Raber <aryeh.raber@gmail.com>
2023-05-31 15:32:09 +0200

    Clean up

--- src/ColorExtractorServiceProvider.php
+++ src/ColorExtractorServiceProvider.php
@@ -9,15 +9,4 @@ class ColorExtractorServiceProvider extends AddonServiceProvider
     protected $modifiers = [
         ColorExtractorModifier::class,
     ];
-
-    public function boot()
-    {
-        parent::boot();
-
-        $this->mergeConfigFrom(__DIR__.'/../config/color_extractor.php', 'color_extractor');
-
-        $this->publishes([
-            __DIR__.'/../config/color_extractor.php' => config_path('color_extractor.php'),
-        ], 'config');
-    }
 }
